bbb2c6789ab08dfdc8aba82a94266ffed085adc8
k3mystra <programmer.coleslaw981@slmail.me>
2026-01-20 18:27:14 +0800

    fix: Try a new generated project

--- src/main/java/mmu/sef/fyj/FyjApplication.java
+++ src/main/java/mmu/sef/fyj/FyjApplication.java
@@ -1,4 +1,4 @@
-package fyj;
+package mmu.sef.fyj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/mmu/sef/fyj/FyjApplicationTests.java
+++ src/test/java/mmu/sef/fyj/FyjApplicationTests.java
@@ -1,4 +1,4 @@
-package fyj;
+package mmu.sef.fyj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
