fbafe7a12a6e1946b35e78bd1c25fc460b381b23
k3mystra <programmer.coleslaw981@slmail.me>
2026-01-20 17:52:19 +0800

    feat: Initial commit
    What

---
+++ src/main/java/fyj/FyjApplication.java
@@ -0,0 +1,13 @@
+package fyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fyj/FyjApplicationTests.java
@@ -0,0 +1,13 @@
+package fyj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
